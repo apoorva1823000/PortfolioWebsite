5058e8d472e8c484b802635f30ebefd502712ed8
apoorva <apoorva.me1@gmail.com>
2023-03-09 09:32:10 +0530

    Updates

--- script.js
+++ script.js
@@ -58,3 +58,12 @@ const typed = new Typed('.multiple-text', {
     backDelay: 1000,
     loop: true
 });
+// Mailing
+function sendEmail(){
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var number = document.getElementById("number").value;
+    var subject = document.getElementById("subject").value;
+    var message = document.getElementById("message").value;
+    
+}
